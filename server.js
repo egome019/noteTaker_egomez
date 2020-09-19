7d6872dbd8cd5a363c9daf2e009eef920f559ec0
egomez <egomezfax19@gmail.com>
2020-09-19 16:00:13 -0700

    trying to fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 // tells node we are creating an express server
 const app = express();
 // this is the port
-const port = process.env.port || 7000;
+const PORT = process.env.PORT || 7000;
 // this helps with data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -17,6 +17,6 @@ require("./routes/htmlRoutes")(app);
 
 // listener
 
-app.listen(port, function() {
-    console.log("App listening on port: " + port);
+app.listen(PORT, function() {
+    console.log("App listening on port: " + PORT);
 })
