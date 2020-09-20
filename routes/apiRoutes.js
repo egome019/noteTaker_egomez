584787b5586be2acd789c30be4ca6f9aeb9353d8
egomez <egomezfax19@gmail.com>
2020-09-19 19:33:03 -0700

    added if statement to post to so old notes would not reappear when saving a new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,13 +16,21 @@ module.exports = function(app) {
 
     // this is the post method which adds new notes to the db.json file
     app.post("/api/notes", function(req, res) {
-        allNotesData = database;
-        req.body.id = allNotesData.length;
-        allNotesData.push(req.body);
-        allNotesData=JSON.stringify(allNotesData);
+        if(database.length > 0){
+            console.log(allNotesData)
+            req.body.id = allNotesData.length;
+            allNotesData.push(req.body);
+            allNotesData=JSON.stringify(allNotesData);
+        }else{
+            allNotesData = database;
+            req.body.id = allNotesData.length;
+            allNotesData.push(req.body);
+            allNotesData=JSON.stringify(allNotesData);
+        }
         fs.writeFile("./db/db.json", allNotesData, "utf8", function(err) {
             if (err) throw err;
         })
+        console.log(allNotesData)
         res.json(JSON.parse(allNotesData));
         console.log("POSTED!")
     });
@@ -38,6 +46,7 @@ module.exports = function(app) {
         fs.writeFile("./db/db.json", allNotesData, "utf8", function(err) {
             if (err) throw err;
         })
+        console.log(allNotesData)
         res.json(allNotesData);
         console.log("DELETED!!")
     })
